22366577feef05ab555e9e88dbfe5ea5ee8ceade
lexus600 <LexusToyota2002@gmail.com>
2024-01-19 21:47:13 +0500

    Inital commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+
+let elQuest1 = prompt("Ismizni?")
+let elQuest2 = prompt("Familiyangiz?")
+let elQuest3 = prompt("Yoshingiz?")
+let elQuest4 = prompt("Qayerdansiz?")
+let elQuest5 = prompt("Qayerda o'qiysiz?")
+let elQuest6 = prompt("Qayerda ishlaysiz?")
+let elQuest7 = prompt("Hobby'ingiz nima?")
+
+
+console.log("1.Ismizni?" + elQuest1);
+console.log("2.Familiyangiz?" + elQuest2);
+console.log("3.Yoshingiz?" + elQuest3);
+console.log("4.Qayerdansiz?" + elQuest4);
+console.log("5.Qayerda o'qiysiz?" + elQuest5);
+console.log("6.Qayerda ishliysiz?" + elQuest6);
+console.log("7.Hobby'ingiz nima?" + elQuest7);
